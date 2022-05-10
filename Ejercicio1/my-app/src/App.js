867c6fe9fb064f5a5059463f5aa42982b950bcfa
Amilcar J. Ruiz <98772743+AmilcarJRuiz@users.noreply.github.com>
2022-05-10 15:46:14 +0200

    ejercicio 1 y 2

---
+++ Ejercicio1/my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Logo from "./matrix.jpeg";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={Logo} alt="logo" />;
+    </div>
+  );
+}
+
+export default App;
